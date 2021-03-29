a7858d37a987ce3088ed037e1b118675c9c19018
Manezeu Patricia <pmanezeu@gmail.com>
2021-03-29 22:52:17 +0100

    Created the user model

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,66 @@
+class UsersController < ApplicationController
+    before_action :user_already_signed_in, except: %i[sign_out show]
+  
+    def user_already_signed_in
+      return if session[:current_user].nil?
+  
+      flash[:notice] = 'Already signed in, sign out if you want to reconnect as another user'
+      redirect_to root_path
+      false
+    end
+  
+    def new
+      @user = User.new
+    end
+  
+    def create
+      @user = User.create(sign_up_params)
+      if @user.save
+        session[:current_user] = @user.username
+        session[:current_user_id] = @user.id
+        redirect_to root_path
+      else
+        flash[:notice] = 'This username is already taken, choose another one'
+        render :new
+      end
+    end
+  
+    def show
+      @user = User.find(params[:id])
+    end
+  
+    def sign_in
+      return unless params[:username] && params[:password]
+  
+      @user = User.find_by(username: params[:username])
+      if @user.nil?
+        flash[:error] = 'Incorrect username, please try again'
+        redirect_to sign_in_path
+      elsif @user.password != params[:password]
+        flash[:error] = 'Incorrect password, please try again'
+        redirect_to sign_in_path
+      else
+        session[:current_user] = @user.username
+        session[:current_user_id] = @user.id
+        redirect_to root_path
+      end
+    end
+  
+    def sign_out
+      if session[:current_user].nil?
+        flash[:notice] = 'Thanks for visiting!'
+        redirect_to sign_in_path
+      else
+        session[:current_user] = nil
+        session[:current_user_id] = nil
+        redirect_to root_path
+      end
+    end
+  
+    private
+  
+    def sign_up_params
+      params.require(:user).permit(:username, :password)
+    end
+  end
+  
